406ab32f91da454fc0aceaf4c596a5298cd59dac
IlashaDogoni <fistonfire@mail.ru>
2024-10-22 15:56:15 +0600

    Refactoring.

---
+++ BarcodeScanner/Screens/BarcodeScanner/BarcodeScannerViewModel.swift
@@ -0,0 +1,22 @@
+//
+//  BarcodeScannerViewModel.swift
+//  BarcodeScanner
+//
+//  Created by Ilya Kokorin on 22.10.2024.
+//
+
+import SwiftUI
+
+final class BarcodeScannerViewModel: ObservableObject {
+    
+    @Published var scannedCode = ""
+    @Published var alertItem: AlertItem?
+    
+    var statusText: String {
+        scannedCode.isEmpty ? "Not Yet Scanned" : scannedCode
+    }
+    
+    var statusTextColor: Color {
+        scannedCode.isEmpty ? .red : .green
+    }
+}
---
+++ BarcodeScanner/Utilities/Alert.swift
@@ -0,0 +1,25 @@
+//
+//  Alert.swift
+//  BarcodeScanner
+//
+//  Created by Ilya Kokorin on 22.10.2024.
+//
+
+import SwiftUI
+
+struct AlertItem : Identifiable {
+    let id =  UUID()
+    let title: String
+    let message : String
+    let dismissButton: Alert.Button
+}
+
+struct AlertContext {
+    static let invalidDeviceInput = AlertItem(title: "Invalid Device Input",
+                                              message: "Something is wrong with the camera. The input cannot be captured",
+                                              dismissButton: .default(Text("OK")))
+    
+    static let invalidScannedValue = AlertItem(title: "Invalid Scanned Value",
+                                               message: "The value scanned is not valid. This app scans EAN-8 and EAN-13",
+                                               dismissButton: .default(Text("OK")))
+}
