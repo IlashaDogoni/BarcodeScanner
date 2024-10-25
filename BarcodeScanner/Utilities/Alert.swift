eef7b7d23d548c9137a6c76feb391287e3df98b8
IlashaDogoni <fistonfire@mail.ru>
2024-10-25 15:57:37 +0600

    a bit of refactoring

--- BarcodeScanner/Utilities/Alert.swift
+++ BarcodeScanner/Utilities/Alert.swift
@@ -9,17 +9,17 @@ import SwiftUI
 
 struct AlertItem : Identifiable {
     let id =  UUID()
-    let title: String
-    let message : String
+    let title: Text
+    let message : Text
     let dismissButton: Alert.Button
 }
 
 struct AlertContext {
-    static let invalidDeviceInput = AlertItem(title: "Invalid Device Input",
-                                              message: "Something is wrong with the camera. The input cannot be captured",
+    static let invalidDeviceInput = AlertItem(title: Text("Invalid Device Input"),
+                                              message: Text("Something is wrong with the camera. The input cannot be captured"),
                                               dismissButton: .default(Text("OK")))
     
-    static let invalidScannedValue = AlertItem(title: "Invalid Scanned Value",
-                                               message: "The value scanned is not valid. This app scans EAN-8 and EAN-13",
+    static let invalidScannedValue = AlertItem(title: Text("Invalid Scanned Value"),
+                                               message: Text("The value scanned is not valid. This app scans EAN-8 and EAN-13"),
                                                dismissButton: .default(Text("OK")))
 }
